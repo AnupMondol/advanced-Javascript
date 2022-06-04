fe82eac433d6abc60778725b901446f78a243278
AnupMondol <anupmondol007@gmail.com>
2022-06-04 14:32:21 +0600

    truefalse

---
+++ truefalse.js
@@ -0,0 +1,25 @@
+// falsy
+
+// const nameee = "";
+// const nameee = 0;
+// let nameee;
+// let nameee = null;
+// let nameee = NaN;
+// const nameee = false;
+
+// truthy
+
+// const nameee = "0";
+// const nameee = " ";
+// const nameee = [];
+// const nameee = {};
+const nameee = true;
+
+
+
+
+if(nameee){
+    console.log("condition is true")
+}else{
+    console.log("condition is false")
+}
